f695250801700ad315488ead541140e481d7fa87
JohanBrun <johannicbrun@gmail.com>
2023-12-18 20:05:10 +0100

    Fix build

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,25 +1,24 @@
 import React, { FC, ReactNode } from 'react';
 
-import './globals.css'
+import './globals.css';
 import { Header } from './header/Header';
 
 type Props = {
   children: ReactNode;
-  title?: string;
 };
 
-const RootLayout: FC<Props> = ({ children, title = 'Mexi' }) => {
+const RootLayout: FC<Props> = ({ children }) => {
   return (
     <html lang="no">
       <head>
-        <title>{title}</title>
+        <title>Mexi</title>
       </head>
       <body>
-      <Header />
-      <main className="flex min-h-screen flex-col items-center justify-between p-24">{children}</main>
-      <footer>
-        {/* Your footer content goes here */}
-      </footer>
+        <Header />
+        <main className="flex min-h-screen flex-col items-center justify-between p-24">
+          {children}
+        </main>
+        <footer>{/* Your footer content goes here */}</footer>
       </body>
     </html>
   );
