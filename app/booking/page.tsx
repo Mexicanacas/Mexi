26d5531238b0ac345db5276e7012621b88d1c6ac
JohanBrun <johannicbrun@gmail.com>
2024-01-28 17:38:00 +0100

    Fix things

--- app/booking/page.tsx
+++ app/booking/page.tsx
@@ -1,10 +1,8 @@
-import './booking.css';
-
 const Booking = () => {
   return (
     <div className="full-page-component bg-booking p-12">
       <section className="bg-mexi-black bg-opacity-50 p-4">
-        <p className="">
+        <p>
           Hva gjør vi? Vi underholder med skjødesløs Latin-Amerikansk-inspirert
           musikk til alles forlystelse. Vi er det unaturlige førstevalget for
           enhver som ønsker prima underholdning. Los Mexicanacas består av ca 15
